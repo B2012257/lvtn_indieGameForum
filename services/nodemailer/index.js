aaa148f4f148f1100219e3279ca4dc6210622e08
ThaiPham <thaib2012257@student.ctu.edu.vn>
2024-05-01 23:44:24 +0700

    docs : Xong danh mục bảng.

--- services/nodemailer/index.js
+++ services/nodemailer/index.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
         user: 'thaistar1998@gmail.com',
-        pass: 'tail sijh iczv sctk'
+        pass: 'mwau feve dnbs yuwc'
     }
 });
 
